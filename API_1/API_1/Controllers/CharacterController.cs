2fc5db53c30c7f120e5c6de66e2121b4fe956434
Sanjsigan <sanjsi0507@gmail.com>
2020-10-05 11:41:01 +0530

    Web API practice

---
+++ API_1/API_1/Controllers/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using API_1.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController:ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Name="Sanjsi"}
+        };
+
+        public  IActionResult Get()
+        {
+            return Ok(characters);
+        }
+    }
+}
